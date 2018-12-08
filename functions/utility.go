adf8f6f1085ad2e81e7ab507900825517f04e07e
zackwolf <azhar.zakiy@gmail.com>
2018-12-08 22:45:40 +0800

    Add Utility Function

---
+++ functions/utility.go
@@ -0,0 +1,29 @@
+package functions
+
+func GetResponse(response_code string, response_message string) interface{} {
+	type Response struct {
+		Response_code    string `json:"response_code"`
+		Response_message string `json:"response_message"`
+	}
+
+	result := Response{
+		response_code,
+		response_message,
+	}
+	return result
+}
+
+func GetResponseWithData(response_code string, response_message string, response_data interface{}) interface{} {
+	type Response struct {
+		Response_code    string      `json:"response_code"`
+		Response_message string      `json:"response_message"`
+		Response_data    interface{} `json:"response_data"`
+	}
+
+	result := Response{
+		response_code,
+		response_message,
+		response_data,
+	}
+	return result
+}
