2591c41db8168cb24d3d4b85b8dbdddf3b4137dd
zackwolf <azhar.zakiy@gmail.com>
2018-12-10 15:58:52 +0800

    Fix camelcase variable

--- controllers/sysRoleController.go
+++ controllers/sysRoleController.go
@@ -12,51 +12,51 @@ import (
 var db = database.ConnectDB()
 
 func ListRole(context *gin.Context) {
-	var sys_role []models.Sys_role
-	if err := db.Find(&sys_role).Error; err != nil {
+	var sysRole []models.Sys_role
+	if err := db.Find(&sysRole).Error; err != nil {
 		context.JSON(http.StatusOK, functions.GetResponseWithData("01", "GET LIST ROLE GAGAL", err))
 	} else {
-		context.JSON(http.StatusOK, functions.GetResponseWithData("00", "GET LIST ROLE SUKSES", sys_role))
+		context.JSON(http.StatusOK, functions.GetResponseWithData("00", "GET LIST ROLE SUKSES", sysRole))
 	}
 }
 
 func InsertRole(context *gin.Context) {
-	var sys_role models.Sys_role
-	err := context.Bind(&sys_role)
+	var sysRole models.Sys_role
+	err := context.Bind(&sysRole)
 	if err != nil {
 		log.Fatal("Error Binding")
 	}
-	if err := db.Create(&sys_role).Error; err != nil {
+	if err := db.Create(&sysRole).Error; err != nil {
 		context.JSON(http.StatusOK, functions.GetResponseWithData("01", "TAMBAH ROLE GAGAL", err))
 	} else {
-		context.JSON(http.StatusOK, functions.GetResponseWithData("00", "TAMBAH ROLE SUKSES", sys_role))
+		context.JSON(http.StatusOK, functions.GetResponseWithData("00", "TAMBAH ROLE SUKSES", sysRole))
 	}
 
 }
 
 func UpdateRole(context *gin.Context) {
-	var sys_role models.Sys_role
+	var sysRole models.Sys_role
 	id := context.Params.ByName("sysrole_kode")
-	if err := db.Where("sysrole_kode = ?", id).First(&sys_role).Error; err != nil {
+	if err := db.Where("sysrole_kode = ?", id).First(&sysRole).Error; err != nil {
 		context.JSON(http.StatusOK, functions.GetResponseWithData("01", "UPDATE ROLE GAGAL", err))
 		return
 	}
-	err := context.Bind(&sys_role)
+	err := context.Bind(&sysRole)
 	if err != nil {
 		log.Fatal("Error Binding")
 	}
-	if err := db.Save(&sys_role).Error; err != nil {
+	if err := db.Save(&sysRole).Error; err != nil {
 		context.JSON(http.StatusOK, functions.GetResponseWithData("01", "UPDATE ROLE GAGAL", err))
 	} else {
-		context.JSON(http.StatusOK, functions.GetResponseWithData("00", "UPDATE ROLE SUKSES", sys_role))
+		context.JSON(http.StatusOK, functions.GetResponseWithData("00", "UPDATE ROLE SUKSES", sysRole))
 	}
 
 }
 
 func DeleteRole(context *gin.Context) {
-	var sys_role models.Sys_role
+	var sysRole models.Sys_role
 	id := context.Params.ByName("sysrole_kode")
-	var result = db.Where("sysrole_kode = ?", id).Delete(&sys_role)
+	var result = db.Where("sysrole_kode = ?", id).Delete(&sysRole)
 	if err := result.Error; err != nil {
 		context.JSON(http.StatusOK, functions.GetResponseWithData("01", "DELETE ROLE GAGAL", err))
 	} else {
