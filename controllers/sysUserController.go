b262e417abfdc1142f16572050e98dd42a22f439
zackwolf <azhar.zakiy@gmail.com>
2018-12-10 16:00:38 +0800

    Fix camelcase variable

--- controllers/sysUserController.go
+++ controllers/sysUserController.go
@@ -9,52 +9,52 @@ import (
 )
 
 func ListUser(context *gin.Context) {
-	var sys_user []models.Sys_user
-	if err := db.Model(&sys_user).Select("sysuser_id,sys_role.sysrole_kode, sysrole_nama, sysuser_nama, sysuser_namalengkap, sysuser_email").Joins("JOIN sys_role ON sys_role.sysrole_kode = sys_user.sysrole_kode").Scan(&sys_user).Error; err != nil {
+	var sysUser []models.Sys_user
+	if err := db.Model(&sysUser).Select("sysuser_id,sys_role.sysrole_kode, sysrole_nama, sysuser_nama, sysuser_namalengkap, sysuser_email").Joins("JOIN sys_role ON sys_role.sysrole_kode = sys_user.sysrole_kode").Scan(&sysUser).Error; err != nil {
 		context.JSON(http.StatusOK, functions.GetResponseWithData("01", "GET LIST USER GAGAL", err))
 	} else {
-		context.JSON(http.StatusOK, functions.GetResponseWithData("00", "GET LIST USER SUKSES", sys_user))
+		context.JSON(http.StatusOK, functions.GetResponseWithData("00", "GET LIST USER SUKSES", sysUser))
 	}
 }
 
 func InsertUser(context *gin.Context) {
-	var sys_user models.Sys_user
-	err := context.Bind(&sys_user)
-	sys_user.Sysuser_passw = functions.CreateHash(sys_user.Sysuser_passw)
+	var sysUser models.Sys_user
+	err := context.Bind(&sysUser)
+	sysUser.Sysuser_passw = functions.CreateHash(sysUser.Sysuser_passw)
 	if err != nil {
 		log.Fatal("Error Binding")
 	}
-	if err := db.Create(&sys_user).Error; err != nil {
+	if err := db.Create(&sysUser).Error; err != nil {
 		context.JSON(http.StatusOK, functions.GetResponseWithData("01", "TAMBAH USER GAGAL", err))
 	} else {
-		context.JSON(http.StatusOK, functions.GetResponseWithData("00", "TAMBAH USER SUKSES", sys_user))
+		context.JSON(http.StatusOK, functions.GetResponseWithData("00", "TAMBAH USER SUKSES", sysUser))
 	}
 
 }
 
 func UpdateUser(context *gin.Context) {
-	var sys_user models.Sys_user
+	var sysUser models.Sys_user
 	id := context.Params.ByName("sysuser_id")
-	if err := db.Where("sysuser_id = ?", id).First(&sys_user).Error; err != nil {
+	if err := db.Where("sysuser_id = ?", id).First(&sysUser).Error; err != nil {
 		context.JSON(http.StatusOK, functions.GetResponseWithData("01", "UPDATE USER GAGAL", err))
 		return
 	}
-	err := context.Bind(&sys_user)
+	err := context.Bind(&sysUser)
 	if err != nil {
 		log.Fatal("Error Binding")
 	}
-	if err := db.Save(&sys_user).Error; err != nil {
+	if err := db.Save(&sysUser).Error; err != nil {
 		context.JSON(http.StatusOK, functions.GetResponseWithData("01", "UPDATE USER GAGAL", err))
 	} else {
-		context.JSON(http.StatusOK, functions.GetResponseWithData("00", "UPDATE USER SUKSES", sys_user))
+		context.JSON(http.StatusOK, functions.GetResponseWithData("00", "UPDATE USER SUKSES", sysUser))
 	}
 
 }
 
 func DeleteUser(context *gin.Context) {
-	var sys_user models.Sys_user
+	var sysUser models.Sys_user
 	id := context.Params.ByName("sysuser_id")
-	var result = db.Where("sysuser_id = ?", id).Delete(&sys_user)
+	var result = db.Where("sysuser_id = ?", id).Delete(&sysUser)
 	if err := result.Error; err != nil {
 		context.JSON(http.StatusOK, functions.GetResponseWithData("01", "DELETE USER GAGAL", err))
 	} else {
