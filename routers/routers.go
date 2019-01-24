265a7a4023d1748e9edd725c4f9dcff435c7cfa4
zackwolf <azhar.zakiy@gmail.com>
2019-01-24 10:09:50 +0800

    JWT Move into folder security

--- routers/routers.go
+++ routers/routers.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"../controllers"
 	"../functions"
+	"../security"
 	"github.com/gin-gonic/gin"
 	"os"
 )
@@ -16,27 +17,27 @@ func SetupRouter() *gin.Engine {
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 
-	r.GET("/listRole", controllers.AuthJWT, controllers.ListRole)
-	r.POST("/insertRole", controllers.AuthJWT, controllers.InsertRole)
-	r.PUT("/updateRole/:sysrole_kode", controllers.AuthJWT, controllers.UpdateRole)
-	r.DELETE("/deleteRole/:sysrole_kode", controllers.AuthJWT, controllers.DeleteRole)
+	r.GET("/listRole", security.AuthJWT, controllers.ListRole)
+	r.POST("/insertRole", security.AuthJWT, controllers.InsertRole)
+	r.PUT("/updateRole/:sysrole_kode", security.AuthJWT, controllers.UpdateRole)
+	r.DELETE("/deleteRole/:sysrole_kode", security.AuthJWT, controllers.DeleteRole)
 
-	r.GET("/listModul", controllers.AuthJWT, controllers.ListModul)
-	r.POST("/insertModul", controllers.AuthJWT, controllers.InsertModul)
-	r.PUT("/updateModul/:sysmodul_kode", controllers.AuthJWT, controllers.UpdateModul)
-	r.DELETE("/deleteModul/:sysmodul_kode", controllers.AuthJWT, controllers.DeleteModul)
+	r.GET("/listModul", security.AuthJWT, controllers.ListModul)
+	r.POST("/insertModul", security.AuthJWT, controllers.InsertModul)
+	r.PUT("/updateModul/:sysmodul_kode", security.AuthJWT, controllers.UpdateModul)
+	r.DELETE("/deleteModul/:sysmodul_kode", security.AuthJWT, controllers.DeleteModul)
 
-	r.POST("/insertRmodul", controllers.AuthJWT, controllers.InsertRmodul)
-	r.DELETE("/deleteRmodul/:kodeRole/:kodeModul", controllers.AuthJWT, controllers.DeleteRmodul)
+	r.POST("/insertRmodul", security.AuthJWT, controllers.InsertRmodul)
+	r.DELETE("/deleteRmodul/:kodeRole/:kodeModul", security.AuthJWT, controllers.DeleteRmodul)
 
-	r.GET("/listUser", controllers.AuthJWT, controllers.ListUser)
-	r.POST("/insertUser", controllers.AuthJWT, controllers.InsertUser)
-	r.PUT("/updateUser/:sysuser_id", controllers.AuthJWT, controllers.UpdateUser)
-	r.DELETE("/deleteUser/:sysuser_id", controllers.AuthJWT, controllers.DeleteUser)
+	r.GET("/listUser", security.AuthJWT, controllers.ListUser)
+	r.POST("/insertUser", security.AuthJWT, controllers.InsertUser)
+	r.PUT("/updateUser/:sysuser_id", security.AuthJWT, controllers.UpdateUser)
+	r.DELETE("/deleteUser/:sysuser_id", security.AuthJWT, controllers.DeleteUser)
 
-	r.POST("/verifyLogin", controllers.AuthJWT, controllers.VerifyLogin)
-	r.POST("/changePassword", controllers.AuthJWT, controllers.ChangePassword)
-	r.GET("/createToken", controllers.CreateTokenEndpoint)
+	r.POST("/verifyLogin", security.AuthJWT, controllers.VerifyLogin)
+	r.POST("/changePassword", security.AuthJWT, controllers.ChangePassword)
+	r.GET("/createToken", security.CreateTokenEndpoint)
 
 	return r
 }
