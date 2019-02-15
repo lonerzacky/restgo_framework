c0e7bc2b5ebda4b8f6cb670fefb99f2032a0a117
zackwolf <azhar.zakiy@gmail.com>
2019-02-15 18:29:56 +0700

    custom database ip

--- database/connection.go
+++ database/connection.go
@@ -15,7 +15,7 @@ func ConnectDB() *gorm.DB {
 		log.Fatal("Error loading .env file")
 	}
 	db, err := gorm.Open(os.Getenv("DB_CONNECTION"), fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("APP_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
+		os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("APP_DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE")))
 	if err != nil {
 		panic(err)
 	}
